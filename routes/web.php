a95b5e79b29637425c9a651384d9a447bbbbc20d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-11-15 21:01:57 -0300

    definindo rotas com Laravel
    Utilizando Route Facade para acessar a classe responsável por gerenciar as rotas HTTP da aplicação

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::any('/', function () {
+    return 'welcome';
 });
+
+Route::match(['get', 'post'], '/dashboard', function() {
+    return 'dashboard';
+});
+
+Route::get('/users', fn() => ['luiz', 'felipe']
+);
+
+Route::redirect('/', 'dashboard');
