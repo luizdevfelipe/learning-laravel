417c0a16a2457b1a3d98812b69e4b00de83e0cf9
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-11-19 16:45:02 -0300

    DI e parâmetros de rota tipados
    Usando a injeção de dependências para passar classes como Request dentro das funções de rota e analisando várias possibilidades de tipagem de parâmetros de rotas, com métodos, globalmente e com Enums

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,25 @@
 <?php
 
+use App\Enums\FileType;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/transactions/{transactionId}', function($transactionId) {
+Route::get('/transactions/{transactionId}', function (int $transactionId) {
     return  'The id of the current transaction is ' . $transactionId;
 });
 
-Route::get('/transactions/{transactionId}/files/{fileId}', function($id, $fileId) {
-    return  'The id of the current transaction is ' . $id . ' and the file accessed is ' . $fileId;
-});
+Route::get('/transactions/{transactionId}/files/{fileType}', function (int $id, FileType $fileType) {
+    return  'The id of the current transaction is ' . $id . ' and the file accessed is ' . $fileType->value;
+});//->whereIn('fileType', ['receipt', 'statement']);
 
-Route::get('/report/{year}/{month?}', function($year, $month = 'x') {
-    return  'Generating report for ' . $year . '/' . $month;
-});
+Route::get('/report/{reportId}', function (Request $request, int $reportId) {
+    //http://localhost/report/22?year=2024&month=11
+    $year = $request->get('year');
+    $month = $request->get('month');
+
+    return  'Generating report ' . $reportId . ' for ' . $year . '/' . $month;    
+})->where('reportId', '[0-9]+');
