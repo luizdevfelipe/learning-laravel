9d17e35ce9c3239ee62ecec63c37eb09d3fcbba0
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-11-26 16:57:19 -0300

    Extraindo rotas para arquivos próprios
    Através da criação de um novo arquivo e informando o Laravel de sua existência é possível extrair rotas comuns do arquivo padrão web.php em casos onde a aplicação toma grandes proporções

---
+++ routes/transactions.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ProcessTransactionController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(TransactionController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('/create', 'create')->name('create');
+    Route::get('/{transactionId}', 'show')->name('show');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{transactionId}/documents', 'documents')->name('documents');
+});
+
+Route::get('/{transactionId}/process', ProcessTransactionController::class)->name('process');
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\DocumentController;
-use App\Http\Controllers\ProcessTransactionController;
-use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,15 +8,3 @@ Route::get('/', function () {
 });
 
 Route::get('/documents', [DocumentController::class, 'index'])->name('documents');
-
-Route::name('transactions.')->prefix('/transactions')->group(function () {
-    Route::controller(TransactionController::class)->group(function () {
-        Route::get('/', 'index')->name('home');
-        Route::get('/create', 'create')->name('create');
-        Route::get('/{transactionId}', 'show')->name('show');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{transactionId}/documents', 'documents')->name('documents');
-    });
-
-    Route::get('/{transactionId}/process', ProcessTransactionController::class);
-});
