ccedb7a8b9abc48d9d785a00bfed7081abc85e67
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-11-29 16:39:26 -0300

    middlewares específicos de rotas
    Em casos onde uma rota pode necessitar de uma proteção ou verificação adicional é possível adicionar um ou mais middleware à ela atravé do método middleware() acorrentado na rota

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DocumentController;
+use App\Http\Middleware\CheckUserRole;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/documents', [DocumentController::class, 'index'])->name('documents');
+
+Route::get('/administration', function() {
+    return 'Secret Admin Page';
+})->middleware(CheckUserRole::class);
