1bbba28317f115202fd1747caea7b6fe038d7c6e
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-04-15 16:07:44 -0300

    método para criar uma nova transaction no banco

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -2,13 +2,28 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
+use Illuminate\Database\DatabaseManager;
+
 class TransactionService
 {
+    public function __construct(public readonly DatabaseManager $db)
+    {
+    }
+
     public function findTransaction(int $transactionId): array
     {
         return [
             'transactionId' => $transactionId,
             'amount' => 25,
         ];
+    }
+
+    public function create(float $amount, Carbon $date, string $description): bool
+    {
+        return $this->db->insert(
+            'INSERT INTO transactions (amount, transaction_date, description, created_at, updated_at) values (?, ?, ?, NOW(), NOW())', 
+            [$amount, $date, $description]
+        );
     }
 }
