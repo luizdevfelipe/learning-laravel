86b9849486394a59a98eb3d2d6614134dac58d56
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-04-17 16:16:50 -0300

    rotas para editar, apagar e visualizar transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Contracts\PaymentProcessor;
 use App\Services\TransactionService;
 use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 
 class TransactionController extends Controller
 {
     public function __construct(
         private readonly TransactionService $transactionService,
+        private readonly Redirector $redirect,
     ) {}
 
     public function index(): View
@@ -19,20 +21,41 @@ class TransactionController extends Controller
         $transactions = $this->transactionService->getAll();
 
         return view('transactions', [
-            'totalIncome' => 50000,
+            'totalIncome'  => 50000,
             'totalExpense' => 45000,
-            'netSaving' => 5000,
-            'goal' => 7500,
+            'netSaving'    => 5000,
+            'goal'         => 7500,
+            'transactions' => $transactions,
         ]);
     }
 
-    public function show(int $transactionId, PaymentProcessor $paymentProcessor): string
+    public function edit(int $transactionId): View
     {
-        $transaction = $this->transactionService->findTransaction($transactionId);
+        $transaction = $this->transactionService->find($transactionId);
 
-        $paymentProcessor->process($transaction);
+        return view('transactions.edit', [
+            'date'        => $transaction->date,
+            'amount'      => $transaction->amount,
+            'description' => $transaction->description,
+        ]);
+    }
 
-        return 'Transaction: ' . $transaction['transactionId'] . ', ' . $transaction['amount'];
+    public function update(int $transactionId, Request $request)
+    {
+        $amount = $request->get('transaction_amount');
+        $date = $request->get('transaction_date');
+        $description = $request->get('transaction_description');
+
+        $this->transactionService->update($transactionId, $amount, new Carbon($date), $description);
+
+        return $this->redirect->to(route('transactions.index'));
+    }
+
+    public function destroy(int $transactionId): RedirectResponse
+    {
+        $this->transactionService->delete($transactionId);
+
+        return $this->redirect->to(route('transactions.index'));
     }
 
     public function create(): View
@@ -40,7 +63,7 @@ class TransactionController extends Controller
         return view('transactions.create');
     }
 
-    public function store(Request $request): string
+    public function store(Request $request): RedirectResponse
     {
         $amount = $request->get('transaction_amount');
         $date = $request->get('transaction_date');
@@ -48,6 +71,6 @@ class TransactionController extends Controller
 
         $this->transactionService->create($amount, new Carbon($date), $description);
 
-        return redirect(route('transactions.index'));
+        return $this->redirect->to(route('transactions.index'));
     }
 }
